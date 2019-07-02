38fd759338cc6e1ea43a0114df79fc15cf2a9b94
ansonsiva <ansonsiva@gmail.com>
2019-07-02 15:25:03 +0800

    change xib to storyboard

--- PutDownMyMac/AppDelegate.swift
+++ PutDownMyMac/AppDelegate.swift
@@ -2,25 +2,173 @@
 //  AppDelegate.swift
 //  PutDownMyMac
 //
-//  Created by Jun Zheng on 2019/7/2.
+//  Created by Jun Zheng on 2019/6/27.
 //  Copyright © 2019 Jun Zheng. All rights reserved.
 //
 
 import Cocoa
+import IOKit
+import IOKit.pwr_mgt
+import IOKit.ps
+import AVFoundation
+import AudioToolbox
+import CoreAudio
+
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
+    var statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+    var intervalTimer:Timer?
+    var player: AVAudioPlayer?
+    let icon = NSImage(named: "record")
+    var isAlertOn = false
+    var originVolume = Float32(0.0)
+    var mainVolume = Float32(0.0)
+    
+    enum batteryStatus {
+        case battery
+        case ac
+    }
+    
+    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var AlertButton: NSMenuItem!
+    @IBOutlet weak var PreferencesButton: NSMenuItem!
+    @IBOutlet weak var QuitButton: NSMenuItem!
+    @IBOutlet weak var AlertMenu: NSMenu!
+    
+    @IBAction func AlertToggle(_ sender: Any) {
+        if getPowerStatus() == .ac {
+            isAlertOn.toggle()
+            noSleep()
+            setAlert(AlertState: isAlertOn)
+        }else{
+            if !isAlertOn {
+                let _ = showAlertPopWindow(question: "Please plug in the AC", text: "Plug in the AC to use the alert")
+            }
+        }
+    }
+    
+    @IBAction func PreferencesPressed(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func QuitPressed(_ sender: Any) {
+        setAudioVolume(volume: originVolume)
+        NSApplication.shared.terminate(self)
+    }
+    
+    
+    fileprivate func setAlert(AlertState isAlertOn:Bool){
+        AlertButton.title = isAlertOn ? "Alert Off":"Alert On"
+        statusItem.button?.image = NSImage.init(named: isAlertOn ? "recording":"record")
+        if isAlertOn {
+            
+            intervalTimer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(showAlert), userInfo: nil, repeats: true)
+            
+        }else{
+            intervalTimer?.invalidate()
+            if mainVolume != Float32(1.0){
+                setAudioVolume(volume: originVolume)
+            }
+        }
+    }
+    
+    fileprivate func getPowerStatus() -> batteryStatus {
+        let remainingSeconds: CFTimeInterval = IOPSGetTimeRemainingEstimate()
+        switch remainingSeconds {
+        case -2.0:
+            return batteryStatus.ac
+        default:
+            return batteryStatus.battery
+        }
+    }
+    
+    fileprivate func setAudioVolume(volume:Float32) {
+        //获取音频设备
+        
+        var defaultOutputDeviceID = AudioDeviceID(0)
+        var defaultOutputDeviceIDSize = UInt32(MemoryLayout.size(ofValue: defaultOutputDeviceID))
+        
+        var getDefaultOutputDevicePropertyAddress = AudioObjectPropertyAddress(
+            mSelector: kAudioHardwarePropertyDefaultOutputDevice,
+            mScope: kAudioObjectPropertyScopeGlobal,
+            mElement: AudioObjectPropertyElement(kAudioObjectPropertyElementMaster))
+        
+        AudioObjectGetPropertyData(AudioObjectID(kAudioObjectSystemObject),&getDefaultOutputDevicePropertyAddress,
+                                   0,nil,&defaultOutputDeviceIDSize,&defaultOutputDeviceID)
+        
+        
+        mainVolume = volume
+        var volumeSize = UInt32(MemoryLayout.size(ofValue: volume))
+        
+        var volumePropertyAddress = AudioObjectPropertyAddress(mSelector:kAudioHardwareServiceDeviceProperty_VirtualMasterVolume,mScope: kAudioDevicePropertyScopeOutput,mElement: kAudioObjectPropertyElementMaster)
+        //获取原音量
+        AudioObjectGetPropertyData(defaultOutputDeviceID, &volumePropertyAddress, 0, nil, &volumeSize, &originVolume)
+        //设置主音量
+        AudioObjectSetPropertyData(defaultOutputDeviceID,&volumePropertyAddress,0,nil,volumeSize,&mainVolume)
+    }
+    
+    fileprivate func noSleep() {
+        var assertionID: IOPMAssertionID = 0
+        var sleepDisabled = false
+        
+        func disableScreenSleep(reason: String = "Disabling Screen Sleep") {
+            sleepDisabled =  IOPMAssertionCreateWithName(kIOPMAssertionTypeNoDisplaySleep as CFString, IOPMAssertionLevel(kIOPMAssertionLevelOn), reason as CFString, &assertionID) == kIOReturnSuccess
+        }
+        
+        func  enableScreenSleep() {
+            IOPMAssertionRelease(assertionID)
+            sleepDisabled = false
+        }
+        
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        AlertButton.title = "Alert On"
+        icon?.isTemplate = true
+        statusItem.button?.image = icon
+        statusItem.menu = AlertMenu
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        setAudioVolume(volume: originVolume)
     }
-
-
+    
+    
+    @objc func showAlert(){
+        if getPowerStatus() == .battery {
+            setAudioVolume(volume: 1.0)
+            print("your mac is lost")
+            soundPlay()
+        }
+        
+    }
+    
+    fileprivate func soundPlay() {
+        
+        let url = URL(fileURLWithPath: Bundle.main.path(forResource: "alarm", ofType: "wav")!)
+        do {
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: nil)
+            player?.volume = 1
+        } catch _{
+            return
+        }
+        player?.play()
+    }
+    
+    fileprivate func showAlertPopWindow(question: String, text: String) -> Bool {
+        let myPopup: NSAlert = NSAlert()
+        myPopup.messageText = question
+        myPopup.informativeText = text
+        myPopup.alertStyle = NSAlert.Style.warning
+        myPopup.addButton(withTitle: "好的")
+        return myPopup.runModal() == NSApplication.ModalResponse.alertFirstButtonReturn
+    }
+    
+    
+    
 }
+
 
---
+++ README.md
@@ -0,0 +1,20 @@
+# PutDownMyMac--an app to prevent someone take your macbook
+
+## 制作初衷
+> 我自己经常在咖啡厅使用电脑码字或者看期货交易，经常人有三急需要离开电脑，电脑放桌上又有点不放心，全部装包里带去上厕所又嫌麻烦，于是想到了做一个报警的app。
+
+## 使用方法
+> 使用很简单，电脑插入电源，打开软件，打开alert功能。如果有人把电源拔掉，程序就会发出警报音直到重新插入电源。如果不是外放而是使用的耳机，则可以提醒你自己，如果使用了外放，则会让周围的人关注到你电脑这个方向。
+
+## Contributing
+> 练手的小成品，看在开发过程中无数次插拔电源，希望大家多多star。也欢迎大家多多contributing或者提意见。
+
+
+## Purpose of this app
+> I use my mac in coffee shop a lot. When I want to go to restroom, It's not so safe to put my mac on table, also it's a lot trouble to put my mac in bag and bring the bag with me. So I think about making an app to prevent others take my mac.
+
+## How to use
+> It's very simple actually. Plug in the AC, turn on the alert. If someone unplug the power and try to take your mac away, the app will try to play Alarm sound.Be sure to disconnect your wireless headset so that other people in coffee shop will notice someone is taking you mac away.
+
+## Contributing
+> Star is very welcomed and also your advice and contributing is need. Thx.
