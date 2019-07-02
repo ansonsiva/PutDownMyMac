f6255b23212fb2eafc4a8bc4baf37b23b451aa86
ansonsiva <ansonsiva@gmail.com>
2019-07-02 15:10:21 +0800

    Initial Commit

---
+++ PutDownMyMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PutDownMyMac
+//
+//  Created by Jun Zheng on 2019/7/2.
+//  Copyright © 2019 Jun Zheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
